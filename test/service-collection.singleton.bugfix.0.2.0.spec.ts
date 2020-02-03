3dda8e49b2105f9f8349b82bdbc1da9c22de2579
SUN BO <starry@vip.qq.com>
2020-02-03 15:59:50 +0800

    增加测试用例：用类型注册单例时，可能会被重复创建多次的问题

---
+++ test/service-collection.singleton.bugfix.0.2.0.spec.ts
@@ -0,0 +1,40 @@
+/* tslint:disable */
+import { expect } from 'chai'
+import { Injectable } from '../src/injectable'
+import { ServiceCollection } from '../src/service-collection'
+
+describe('ServiceCollection.AddSingleton', function() {
+  it('ServiceCollection.AddSingleton 用类型注册单例时，可能会被重复创建多次的问题', function() {
+    let collection = new ServiceCollection()
+    collection.AddSingleton(BaseService, TestService)
+    collection.AddTransient(TestBiz)
+
+    let bizA = collection.GetService(TestBiz)
+    let bizB = collection.GetService(TestBiz)
+    expect(bizA.Service).is.equal(bizB.Service)
+  })
+})
+
+@Injectable()
+class BaseService {
+  public constructor() {}
+}
+
+@Injectable()
+class TestService extends BaseService {
+  public constructor() {
+    super()
+  }
+}
+
+@Injectable()
+class TestBiz {
+  private service: BaseService
+  constructor(service: BaseService) {
+    this.service = service
+  }
+
+  public get Service(): BaseService {
+    return this.service
+  }
+}
